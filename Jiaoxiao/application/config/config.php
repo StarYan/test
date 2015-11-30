78451e7b64f3849a27d94914f2b92918f46143ca
Administrator_Wen <936818307@qq.com>
2015-11-30 13:25:01 +0800

    改了一下config的base_url

--- Jiaoxiao/application/config/config.php
+++ Jiaoxiao/application/config/config.php
@@ -17,8 +17,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-//$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
-
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/TEST/Jiaoxiao';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/gitTEST/Jiaoxiao';
 /*
 |--------------------------------------------------------------------------
 | Index File
