39315fcc256d8931f64cdfe1a7099d53e01cf862
Administrator_Wen <936818307@qq.com>
2015-12-16 10:29:18 +0800

    appointment表

--- 数据库/appointment.sql
+++ 数据库/appointment.sql
@@ -1,40 +1,44 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1
--- http://www.phpmyadmin.net
---
--- 主机: localhost
--- 生成日期: 2015 年 11 月 29 日 09:20
--- 服务器版本: 5.5.20
--- PHP 版本: 5.3.10
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : localhost
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : jiaxiao
 
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+Date: 2015-12-16 10:27:45
+*/
 
---
--- 数据库: `gitjiaxiao`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `appointment`
---
-
-CREATE TABLE IF NOT EXISTS `appointment` (
+-- ----------------------------
+-- Table structure for `appointment`
+-- ----------------------------
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE `appointment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `coachandplaceid` int(11) NOT NULL COMMENT '教练与场地关系ID',
   `userid` int(11) NOT NULL COMMENT '用户id',
-  `time` date NOT NULL COMMENT '预约时间',
-  `cartype` varchar(128) NOT NULL COMMENT '车型',
+  `carid` bigint(20) NOT NULL COMMENT '车型',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='预约' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='预约';
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of appointment
+-- ----------------------------
+INSERT INTO `appointment` VALUES ('1', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('2', '3', '46', '1');
+INSERT INTO `appointment` VALUES ('3', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('4', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('5', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('6', '9', '46', '1');
+INSERT INTO `appointment` VALUES ('7', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('8', '3', '46', '2');
+INSERT INTO `appointment` VALUES ('9', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('10', '1', '45', '2');
+INSERT INTO `appointment` VALUES ('11', '2', '46', '2');
+INSERT INTO `appointment` VALUES ('12', '1', '45', '1');
